6a42a2049f781965e7f9eb2a009593c11004144e
vborszeki <vborszeki@gmail.com>
2016-11-30 16:58:02 +0100

    refactor rest api

--- config/routing.php
+++ config/routing.php
@@ -6,20 +6,16 @@ function setRouting(Silex\Application $app)
 {
   $app['controller.todo'] = function() use($app)
   {
-    return new TodoController($app['db']);
+    return new TodoController
+    (
+      $app['request_stack']->getCurrentRequest(),
+      $app['db']
+    );
   };
 
   $app->get('', "controller.todo:listTodos");
 
   $app->delete('/api/todo/{id}', "controller.todo:deleteTodo");
 
-  $app->post('/api/todo', function() use($app)
-  {
-    $req = $app['request_stack']->getCurrentRequest();
-    $sql = 'INSERT INTO todo_lista (title, completed) VALUES (?, false)';
-    $title = $req->request->get('title');
-
-    $app['db']->executeQuery($sql, array($title));
-    return 'Success';
-  });
+  $app->post('/api/todo', "controller.todo:insertTodo");
 }
--- html/controllers/todo_controller.php
+++ html/controllers/todo_controller.php
@@ -3,10 +3,12 @@
 class TodoController
 {
   protected $db;
+  protected $request;
 
-  public function __construct($db)
+  public function __construct($request, $db)
   {
     $this->db = $db;
+    $this->request = $request;
   }
 
   public function listTodos()
@@ -25,5 +27,14 @@ class TodoController
     $sql = 'DELETE FROM todo_list WHERE id=?';
     $this->db->executeQuery($sql, array($id));
     return 'success';
+  }
+
+  public function insertTodo()
+  {
+    $sql = 'INSERT INTO todo_list (title, completed) VALUES (?, false)';
+    $title = $this->request->get('title');
+
+    $this->db->executeQuery($sql, array($title));
+    return 'Success';
   }
 }
