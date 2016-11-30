87ca30c98d7e51e72552c55d44d0b23fa0eca93e
Gyorgy Szauer <gszauer@gszauer-mbp.local>
2016-11-30 10:38:34 +0100

    Add api for new Todo item

--- html/routing.php
+++ html/routing.php
@@ -2,7 +2,7 @@
 
 function setRouting(Silex\Application $app)
 {
-  $app->get('/list', function() use ($app)
+  $app->get('', function() use ($app)
   {
     $html = '';
     $todos = $app['db']->fetchAll('SELECT * FROM todo_lista');
@@ -18,5 +18,14 @@ function setRouting(Silex\Application $app)
     $sql = 'DELETE FROM todo_lista WHERE id=?';
     $app['db']->executeQuery($sql, array($id));
     return 'success';
+  });
+
+  $app->post('/api/todo', function() use($app) {
+    $req = $app['request_stack']->getCurrentRequest();
+    $sql = 'INSERT INTO todo_lista (title, completed) VALUES (?, false)';
+    $title = $req->request->get('title');
+
+    $app['db']->executeQuery($sql, array($title));
+    return 'Success';
   });
 }
