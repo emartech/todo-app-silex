bb40250589e4d5ef3ce3fa18ca05a60f457a3a02
Gyorgy Szauer <gszauer@gszauer-mbp.local>
2016-11-30 16:19:59 +0100

    TodoList Entity

---
+++ orm/TodoList.php
@@ -0,0 +1,82 @@
+<?php
+namespace Entities;
+/**
+ * TodoList
+ */
+class TodoList
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var integer
+     */
+    private $completed;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return TodoList
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set completed
+     *
+     * @param integer $completed
+     *
+     * @return TodoList
+     */
+    public function setCompleted($completed)
+    {
+        $this->completed = $completed;
+
+        return $this;
+    }
+
+    /**
+     * Get completed
+     *
+     * @return integer
+     */
+    public function getCompleted()
+    {
+        return $this->completed;
+    }
+}
+
