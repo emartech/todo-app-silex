89808d5911647aacbd80cdac81d580544cbacd39
Gyorgy Szauer <gszauer@gszauer-mbp.local>
2016-11-30 16:36:49 +0100

    Register fix

--- config/register.php
+++ config/register.php
@@ -5,15 +5,13 @@ function registerDb(Silex\Application $app)
 {
   global $dbParams;
 
-  $app->register(new Silex\Provider\ServiceControllerServiceProvider());
-  $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+  $app->register(new Silex\Provider\ServiceControllerServiceProvider);
+  $app->register(new Silex\Provider\DoctrineServiceProvider, array(
     'db.options' => $dbParams
   ));
 
   $app->register(new Dflydev\Provider\DoctrineOrm\DoctrineOrmServiceProvider,
   [
-    'orm.proxies_dir' => 'cache',
-    'orm.auto_generate_proxies' => true,
     'orm.em.options' => [
       'mappings' => [
         [
