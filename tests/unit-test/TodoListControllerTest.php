84949b7079eaa988e1caa4e3eeb2b75d576916ac
Gyorgy Szauer <gszauer@gszauer-mbp.local>
2016-12-02 11:31:28 +0100

    UnitTest update

--- tests/unit-test/TodoListControllerTest.php
+++ tests/unit-test/TodoListControllerTest.php
@@ -1,8 +1,28 @@
 <?php
+require 'html/controllers/todo_controller.php';
+
+class DummyTwigClass {
+    public function render($template, $params){ }
+}
+
 class TodoListControllerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testList()
+    public function testRenderTodos()
     {
-      
+        $twigMock = $this->createMock(DummyTwigClass::class);
+        $controller = new TodoController(null, null, null, $twigMock);
+
+        $form = 'FORMCLASS';
+        $todos = [];
+
+        $twigMock->expects($this->once())
+                 ->method('render')
+                 ->with('index.twig', [
+                     'form' => $form,
+                     'todos' => $todos
+                ]);
+
+        $controller->renderTodos($form, $todos);
+
     }
 }
